a95f1118610ba0dc1e3b550c7c06015d76fa8385
Jimin Jung <jimnn.jung@gmail.com>
2022-05-24 09:25:08 +0900

    Adding molar mass function; helper method to be added later

--- chem.py
+++ chem.py
@@ -20,3 +20,18 @@ class Element:
     def set_mass(self, new_mass):
         """Setter method that allows the user to change the mass."""
         Element.mass_dictionary[self.symbol] = new_mass
+
+# The class representing chemical molecules and compounds. Elements in the compounds must be instantiated before the compound itself.
+class Molecule:
+
+    def __init__(self, symbol):
+        self.symbol = symbol
+        self.molar_mass = calculate_mass(symbol)
+
+    def calculate_mass(self, symbol):
+        """Calculates the molar mass with helper methods."""
+        composition_dict = decompose_symbol(symbol)
+        mass = 0
+        for elem in composition_dict:
+            mass += Element.mass_dictionary[elem]
+        return mass
