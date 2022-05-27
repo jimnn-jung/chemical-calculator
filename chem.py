9e901eb539ebc35f27d5bb1acc1de1ee33fa4efa
Jimin Jung <jimnn.jung@gmail.com>
2022-05-27 10:49:53 +0900

    Completing the split helper function

--- chem.py
+++ chem.py
@@ -24,29 +24,47 @@ class Element:
 # The class representing chemical molecules and compounds. Elements in the compounds must be instantiated before the compound itself.
 class Molecule:
 
-    def __init__(self, symbol):
-        self.symbol = symbol
+    def __init__(self, formula):
+        self.formula = formula
+
+    def get_formula(self):
+        """Getter method of a Molecule's formula."""
+        return self.formula
 
     def calculate_mass(self):
         """Calculates the molar mass with helper methods."""
-        composition_dict = decompose_symbol()
+        composition_dict = decompose_formula()
         mass = 0
         for elem in composition_dict:
             mass += Element.mass_dictionary[elem]
         self.molar_mass = mass
         return mass
 
-    def decompose_symbol(self):
-        """Given the list from split_symbol, organizes the chemical formula into a dictionary with Element symbols as keys and the counts as values."""
-        split_list = split_symbol()
+    def decompose_formula(self):
+        """Given the list from split_formula, organizes the chemical formula into a dictionary with Element symbols as keys and the counts as values."""
+        split_list = split_formula()
         composition = {}
         # Iterate through split_list to add to dictionary
         self.composition = composition
         return composition
 
-    def split_symbol(self):
+    def split_formula(self):
         """Given the chemical formula, returns a list of chunks an Element and its count using RegEx."""
-        symbol_copy = self.symbol
+        formula_copy = self.get_formula()
         chunks = []
-        chunks.extend(re.findall(r"([A-Z][a-z]?\d*)", symbol_copy))
+        open_paren = re.findall(r"\(", formula_copy)
+        close_paren = re.findall(r"\)", formula_copy)
+        if len(open_paren) != len(close_paren):
+            raise Exception("Unmatching numbers of opening and closing parentheses")
+        for i in range(len(open_paren)):
+            polyatomic = re.search(r"\(([A-Z][a-z]?\d*)+\)\d+", formula_copy)
+            if not polyatomic:
+                raise Exception("Invalid polyatomic group in provided formula")
+            poly_group = polyatomic.group()
+            closing = poly_group.index(")")
+            factor = int(poly_group[(closing + 1):])
+            chunks.extend(re.findall(r"([A-Z][a-z]?\d*)", poly_group) * factor)
+            first_opening = formula_copy.index("(")
+            formula_copy = formula_copy[:first_opening] + formula_copy[(first_opening + len(poly_group)):]
+        chunks.extend(re.findall(r"([A-Z][a-z]?\d*)", formula_copy))
         return chunks
--- chem_test.py
+++ chem_test.py
@@ -15,15 +15,19 @@ def test_split_simple():
     """Sees if the program can split formula into the intended lists of chunks."""
     water = Molecule("H2O")
     water_expected = ["H2", "O"]
-    water_actual = water.split_symbol()
+    water_actual = water.split_formula()
     assert water_expected == water_actual, "Incorrect splitting of H2O"
 
     phosphoric = Molecule("H3PO4")
     phosphoric_expected = ["H3", "P", "O4"]
-    phosphoric_actual = phosphoric.split_symbol()
+    phosphoric_actual = phosphoric.split_formula()
     assert phosphoric_expected == phosphoric_actual, "Incorrect splitting of H3PO4"
 
 def test_split_polyatomic():
     """Sees if the program can split formula with parentheses for polyatomic ions into the intended lists of chunks."""
+    calcium_phos = Molecule("Ca3(PO4)2")
+    calcium_phos_expected = ["P", "O4", "P", "O4", "Ca3"]
+    calcium_phos_actual = calcium_phos.split_formula()
+    assert calcium_phos_expected == calcium_phos_actual, "Incorrect splitting of Ca3(PO4)2"
 
 setup_test()
